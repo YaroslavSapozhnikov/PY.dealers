d14d53e86599551e02053de34051b33e9a004515
YaroslavSapozhnikov <yars@narod.ru>
2025-09-17 01:36:52 +0300

    time

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Iterator, IO
-
+from datetime import date, time, datetime, timedelta
 import openpyxl
+# import time
 
 
 def iter_excel(file: IO[bytes]) -> Iterator[dict[str, object]]:
@@ -9,6 +10,18 @@ def iter_excel(file: IO[bytes]) -> Iterator[dict[str, object]]:
     headers = [str(cell.value) for cell in next(rows)]
     for row in rows:
         yield dict(zip(headers, (cell.value for cell in row)))
+
+
+def last_auth(dilers: list[dict]):
+    last_t = 0
+    for dealer in dealers:
+        t = dealer.get('Последняя авторизация')
+        if t != 'None':
+            dt = datetime.strptime(t, "%Y.%m.%d %H:%M:%S")
+            unix_t = int(dt.timestamp())
+            if last_t < unix_t:
+                last_t = unix_t
+    return datetime.fromtimestamp(last_t).strftime('%Y.%m.%d %H:%M:%S')
 
 
 # Press the green button in the gutter to run the script.
@@ -21,6 +34,8 @@ if __name__ == '__main__':
     dealers = [{k: v for k, v in zip(headers, tuple(str(cell.value) for cell in row))} for row in rows]
     for dealer in dealers:
         print(dealer)
+
+    print(last_auth(dealers))
 
     # with open('dealers.xlsx', 'rb') as f:
     #     rows = iter_excel(f)
